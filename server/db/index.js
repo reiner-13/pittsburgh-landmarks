e4dd78b8803d91bdc023f07e1f6d106af40d1d7c
reiner-13 <joereiner13@gmail.com>
2024-12-25 13:00:57 -0500

    Passing new parameters into Pool

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,12 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: 'postgres',
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: 5432
+});
 module.exports = {
     query: (text, params) => pool.query(text, params),
 };
